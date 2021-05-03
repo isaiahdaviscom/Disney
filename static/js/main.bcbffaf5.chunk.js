10d3d5c89a51670a682319928e0fec4cc53bb47f
Isaiah Davis <isaiah.davisx@gmail.com>
2021-05-02 19:50:13 -0500

    Updates

---
+++ static/js/main.bcbffaf5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpstream-app"]=this["webpackJsonpstream-app"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),s=n(3),a=n.n(s),i=(n(8),n(0));var o=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"helHello lodfd"})})};a.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.bcbffaf5.chunk.js.map
